452378b6ee9cf7c1b5bf0610f33a2f90332e978a
Saurabh-kumar-1499 <54003176+Saurabh-kumar-1499@users.noreply.github.com>
2020-10-26 12:37:30 +0530

    Commit 1 by Collaborator
    first change by the collaborator.

--- Base.java
+++ Base.java
@@ -3,5 +3,7 @@ class Base
   public static void main(String args[])
   {
     System.out.println("Hello Collaborator.");
+    System.out.println("Hello Manager this is first change by the collaborator.");
+    
    } 
  }  
